4d284a1d20e84ceb3235c66e96b1619f2af0f4c9
Hiroshi Urabe <mail@torounit.com>
2018-04-20 05:31:15 +0900

    update latest contents

--- wp-content/mu-plugins/pwa/fetch-html.js
+++ wp-content/mu-plugins/pwa/fetch-html.js
@@ -1,5 +1,6 @@
 (function( $ ) {
-	document.addEventListener( "DOMContentLoaded", function( event ) {
+	const selector = '#page';
+	document.addEventListener( "DOMContentLoaded", function() {
 		$( '.error-not-available' ).hide();
 		if ('caches' in window) {
 			console.log( '[fetch-html]', location.href );
@@ -8,9 +9,17 @@
 					console.log( response );
 				}
 			} );
-			$( "body" ).load( location.href + " #page" ).finally( (() => {
+
+			fetch( location.href ).then( ( response ) => {
+				return response.text()
+			} ).then( ( responseText ) => {
+				let $html = jQuery( "<div>" ).append( jQuery.parseHTML( responseText ) );
+				let content = $html.find( selector ).html();
+				$( selector ).html( content );
+				$( 'title' ).text( $html.find( 'title' ).text() )
+			} ).finally( () => {
 				$( '.error-not-available' ).show()
-			}) );
+			} )
 		}
 	} );
 })( jQuery );
