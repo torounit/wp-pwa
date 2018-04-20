3ab40aa3de63e5ceaac9f0e6c7048682f69c7f71
Hiroshi Urabe <mail@torounit.com>
2018-04-20 18:39:51 +0900

    reformat code

--- wp-content/mu-plugins/pwa/pwa.php
+++ wp-content/mu-plugins/pwa/pwa.php
@@ -7,16 +7,16 @@ define( 'MANIFEST_ENDPOINT', 'manifest' );
 define( 'NOT_AVAILABLE_ENDPOINT', 'not-available' );
 define( 'UPDATE_CACHE_QUERY_VAR', 'update-asset-caches' );
 
+
+/**
+ * Add Rewrite rules.
+ */
 add_action( 'init', function () {
 	add_rewrite_endpoint( SERVICE_WORKER_ENDPOINT, EP_ROOT );
 	add_rewrite_endpoint( MANIFEST_ENDPOINT, EP_ROOT );
 	add_rewrite_endpoint( NOT_AVAILABLE_ENDPOINT, EP_ROOT );
 } );
 
-add_action( 'wp_enqueue_scripts', function () {
-	wp_enqueue_script( 'pwa-service-worker', WPMU_PLUGIN_URL . '/pwa/register-service-worker.js' );
-	wp_enqueue_script( 'pwa-fetch-html', WPMU_PLUGIN_URL . '/pwa/fetch-html.js', [], false, true );
-} );
 
 add_filter( 'query_vars', function ( $vars ) {
 	$vars[] = SERVICE_WORKER_ENDPOINT;
@@ -26,14 +26,9 @@ add_filter( 'query_vars', function ( $vars ) {
 	return $vars;
 } );
 
-add_action( 'wp_head', function () {
-	?>
-	<meta name="theme-color" content="#<?php background_color();?>">
-	<link rel="manifest" href="<?php echo home_url( MANIFEST_ENDPOINT ); ?>">
-	<?php
-} );
-
-
+/**
+ * Controller
+ */
 add_action( 'template_redirect', function () {
 	global $wp_query;
 
@@ -62,12 +57,31 @@ add_action( 'template_redirect', function () {
 	}
 } );
 
-
+/**
+ * Rebuild Static Caches.
+ */
 add_action( 'after_switch_theme', function () {
-	wp_remote_get( add_query_arg( UPDATE_CACHE_QUERY_VAR, '1', home_url() ), array( 'timeout' => 120 ) );
+	wp_remote_get( add_query_arg( UPDATE_CACHE_QUERY_VAR, '1', home_url() ), [ 'timeout' => 120 ] );
 } );
 
-
+/**
+ * Gravatar always https.
+ */
 add_filter( 'get_avatar_url' , function ( $url ) {
 	return preg_replace( '/http:\/\/[0-9]\.gravatar\.com/', 'https://secure.gravatar.com', $url );
 });
+
+/**
+ * Add Head tags
+ */
+add_action( 'wp_head', function () {
+	?>
+	<meta name="theme-color" content="#<?php background_color();?>">
+	<link rel="manifest" href="<?php echo home_url( MANIFEST_ENDPOINT ); ?>">
+	<?php
+} );
+
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_script( 'pwa-service-worker', WPMU_PLUGIN_URL . '/pwa/register-service-worker.js' );
+	wp_enqueue_script( 'pwa-fetch-html', WPMU_PLUGIN_URL . '/pwa/fetch-html.js', [], false, true );
+} );
